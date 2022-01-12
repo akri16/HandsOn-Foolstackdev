9145497d7ec556fb04a7a3ad37db9227898ef402
Amit Krishna A <amit.ananthkumar@gmail.com>
2022-01-12 11:40:17 +0530

    Inject JS into HTML

--- nodejs-express/app.js
+++ nodejs-express/app.js
@@ -11,23 +11,28 @@ app.set('view engine', 'ejs');
 
 // Processes requests from the top to the bottom
 app.get("/", (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-    res.render('about');
+    res.render('about', { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-    res.render('create');
+    res.render('create', { title: "Create" });
 });
 
 // Redirect
 app.get("/about-us", (req, res) => {
-    res.redirect('/about');
+    res.redirect('/about', { title: "About" });
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: "404" });
 });
